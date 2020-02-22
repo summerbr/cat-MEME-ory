7abc81daacb28a53b44fab09a666f0c93694e890
Summer Brown <summer-brown@comcast.net>
2020-02-21 22:41:32 -0600

    Updates

---
+++ precache-manifest.5b89bd377b2fb905de424d43f649da9c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a105d0cdbd778d70b12646998d9111",
+    "url": "/cat-MEME-ory/index.html"
+  },
+  {
+    "revision": "1033bd6ae8aef8a13bef",
+    "url": "/cat-MEME-ory/static/css/main.94616e2d.chunk.css"
+  },
+  {
+    "revision": "0de3d7b368ed2cef646d",
+    "url": "/cat-MEME-ory/static/js/2.d8861b4c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cat-MEME-ory/static/js/2.d8861b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1033bd6ae8aef8a13bef",
+    "url": "/cat-MEME-ory/static/js/main.c2bcea91.chunk.js"
+  },
+  {
+    "revision": "3fbb450485e5089dbaa5",
+    "url": "/cat-MEME-ory/static/js/runtime-main.cc7adc91.js"
+  },
+  {
+    "revision": "c1a812aedd687b98ca4939488b7b20f3",
+    "url": "/cat-MEME-ory/static/media/Kittyface.c1a812ae.ttf"
+  },
+  {
+    "revision": "5b727387d53d53b7571a2bd3d6bc7a3f",
+    "url": "/cat-MEME-ory/static/media/LMS_CAT.5b727387.ttf"
+  },
+  {
+    "revision": "784cc1184c539888e06041632dc817ea",
+    "url": "/cat-MEME-ory/static/media/LMS_CAT.784cc118.svg"
+  },
+  {
+    "revision": "e609fb2fe77e371f88f0a26e762a22f2",
+    "url": "/cat-MEME-ory/static/media/LMS_CAT.e609fb2f.eot"
+  },
+  {
+    "revision": "fba94866f77e8554befc05bcc3bb57f9",
+    "url": "/cat-MEME-ory/static/media/LMS_CAT.fba94866.woff"
+  },
+  {
+    "revision": "7fe7285d85b570fa81ffd4cdbc6c427b",
+    "url": "/cat-MEME-ory/static/media/cat-gf-(526).7fe7285d.cur"
+  },
+  {
+    "revision": "ba41ffbe694a898c9da7a8f2e84f6508",
+    "url": "/cat-MEME-ory/static/media/catcrypt.ba41ffbe.ttf"
+  },
+  {
+    "revision": "162561b861dd69bb1ea78a2ff88df93a",
+    "url": "/cat-MEME-ory/static/media/coffee.162561b8.jpg"
+  },
+  {
+    "revision": "ce5b0b990f0693a92778359c9f84615d",
+    "url": "/cat-MEME-ory/static/media/grumpy-cat-bird.ce5b0b99.jpeg"
+  },
+  {
+    "revision": "d1c96ac90048e609e43c7cc51b3d9225",
+    "url": "/cat-MEME-ory/static/media/haters.d1c96ac9.jpg"
+  },
+  {
+    "revision": "78f6eb672c0c9389514a466fe0b507f1",
+    "url": "/cat-MEME-ory/static/media/meow.78f6eb67.jpeg"
+  },
+  {
+    "revision": "eaf8afe0fb1ff8e769c540829f17f91e",
+    "url": "/cat-MEME-ory/static/media/nike.eaf8afe0.jpeg"
+  },
+  {
+    "revision": "703f50588ee5f514afacf1d6e3ccb1f0",
+    "url": "/cat-MEME-ory/static/media/testes.703f5058.jpeg"
+  },
+  {
+    "revision": "1a77a6bf436a3e0e912f360a76c4c96d",
+    "url": "/cat-MEME-ory/static/media/—Pngtree-illustration-cat-cat-unicorn_3930649.1a77a6bf.png"
+  },
+  {
+    "revision": "6ca4eaaffa695e07a1b5f4c82b9c35c5",
+    "url": "/cat-MEME-ory/static/media/—Pngtree—cute_adorable_cat_vector_pattern_3691386.6ca4eaaf.png"
+  }
+]);
